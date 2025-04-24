6dcb812b7d323d9e55330f7c5cecf04b29bd18ea
Miller Prosser <mcprosse@uchicago.edu>
2025-04-24 10:32:24 -0500

    add repo link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,10 @@
 
 <div class="container">
 	<div>Core Svelte Concepts</div>
+	<p>
+		Visit the GitHub repo: <a
+			class="text-red-800 hover:font-bold"
+			href="https://github.com/MillerPr/svelteCore">svelteCore</a
+		>
+	</p>
 </div>
